d92913311f8b2d57414c810add706b886d853faf
Samuel B <sbiebsam@gmail.com>
2025-12-05 20:41:06 +0100

    removed personal build files

--- build/CMakeFiles/Gearsimulation.dir/src/Gear.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Gearsimulation.dir/src/Gear.cpp.o: \
- /home/samu/Schreibtisch/Projects/gearSimulation/src/Gear.cpp \
- /usr/include/stdc-predef.h \
- /home/samu/Schreibtisch/Projects/gearSimulation/include/Gear.hpp
--- build/CMakeFiles/MyLibrary.dir/src/Gear.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/MyLibrary.dir/src/Gear.cpp.o: \
- /home/samu/Schreibtisch/Projects/gearSimulation/src/Gear.cpp \
- /usr/include/stdc-predef.h \
- /home/samu/Schreibtisch/Projects/gearSimulation/include/Gear.hpp
